ff1184fe2765f6a6797c0a2c74e3115b2163f689
nub31 <oliverlhs@outlook.com>
2024-06-22 17:53:25 +0200

    Welcome message

--- src/lib/components/topbar/Topbar.svelte
+++ src/lib/components/topbar/Topbar.svelte
@@ -1,3 +1,3 @@
-<header class="h-16 bg-light-300 px-6 py-4">
+<header class="h-16 bg-light-300 px-6 py-4 sticky top-0">
 	<img class="h-full" src="/logo-dark.png" alt="Logo" />
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,13 @@
 	import Tabs from '$lib/components/tabs/Tabs.svelte';
 </script>
 
+<div class="flex items-center flex-col my-8">
+	<h1 class="font-medium text-4xl">Welcome!</h1>
+	<div class="text-dark-200 mt-2">
+		I dont really know what to put here, so enjoy some games while i think of something
+	</div>
+</div>
+
 <Tabs class="mt-5 flex flex-col items-center">
 	<div>
 		<Tab label="Pong">
